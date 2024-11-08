4856a4908f25d154edd88d5c3338866966fcb680
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-11-08 15:36:33 -0300

    .

--- app/Console/Commands/CheckReservationForDelays.php
+++ app/Console/Commands/CheckReservationForDelays.php
@@ -2,29 +2,37 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Reservation;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class CheckReservationForDelays extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:check-reservation-for-delays';
+    protected $description = 'Verifica se existe alguma reserva que ultrapassou o limite de entrega.';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+        $datenow = now();
+
+        $reservation = Reservation::where('return_date', '>=', $datenow)
+            ->where('reservation_finished', 'false')
+            ->where('date_finished', 'false')
+            ->get();
+
+        if (empty($reservation)) {
+            Log::info('Nenhuma reserva esta em atraso.');
+        }
+
+        if ($reservation) {
+            foreach ($reservation as $reserve) {
+                Log::warning("Reserva nº{$reserve->id} realizada por {$reserve->delivery_to} está em atraso.");
+            }
+        }
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('stock:check-quantity')->everyMinute();
+Schedule::command('app:check-reservation-for-delays')->hourly();
