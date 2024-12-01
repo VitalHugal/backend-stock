4c920aaf0152d1a2883695af8150de4989dacf7f
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-12-01 00:13:30 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+    'allowed_origins' => ['https://www.estoque.bizsys.com.br'],
 
     'allowed_origins_patterns' => [],
 
