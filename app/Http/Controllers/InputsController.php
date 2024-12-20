5c18d0fedf589945898e10d13153e1dc8b72ce58
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-12-20 17:44:22 -0300

    Update InputsController.php

--- app/Http/Controllers/InputsController.php
+++ app/Http/Controllers/InputsController.php
@@ -151,7 +151,7 @@ class InputsController extends CrudController
                         ? $input->productEquipament->name . ' (Deletado)' // Se deletado (Deletado)
                         : $input->productEquipament->name ?? null,
                     // 'category_name' => $input->productEquipament->category->name ?? null,
-                    'category_name' => $input->productEquipament->category->trashed()
+                    'category_name' => $input->category->trashed()
                         ? $input->productEquipament->category->name . ' (Deletado)' // Se deletado(Deletado)
                         : $input->productEquipament->category->name ?? null,
                     'fk_user_id' => $input->fk_user_id ?? null,
