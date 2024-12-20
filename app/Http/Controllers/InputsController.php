02c595aa2aea45439f59f58451ea5be8a31ce49d
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-12-20 18:14:01 -0300

    Update InputsController.php

--- app/Http/Controllers/InputsController.php
+++ app/Http/Controllers/InputsController.php
@@ -131,11 +131,11 @@ class InputsController extends CrudController
                     //     ? $input->productEquipament->name . ' (Deletado)' // Se deletado(Deletado)
                     //     : $input->productEquipament->name ?? null,
                     
-                    // 'category_name' => $input->productEquipament->category->name ?? null,
-                    
-                    'category_name' => $input->productEquipament->category->trashed()
-                        ? $input->productEquipament->category->name . ' (Deletado)' // Se deletado(Deletado)
-                        : $input->productEquipament->category->name ?? null,
+                    'category_name' => $input->productEquipament->category->name ?? null,
+
+                    // 'category_name' => $input->productEquipament->category->trashed()
+                    //     ? $input->productEquipament->category->name . ' (Deletado)' // Se deletado(Deletado)
+                    //     : $input->productEquipament->category->name ?? null,
                     'fk_user_id' => $input->fk_user_id ?? null,
                     'name_user_input' => $input->user->name ?? null,
                     'created_at' => $this->input->getFormattedDate($input, 'created_at') ?? null,
