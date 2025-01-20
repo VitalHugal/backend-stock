273e27f9e05ecdc84e295b0eced189f3624494b4
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-01-20 14:34:13 -0300

    Update ProductEquipamentController.php

--- app/Http/Controllers/ProductEquipamentController.php
+++ app/Http/Controllers/ProductEquipamentController.php
@@ -315,7 +315,7 @@ class ProductEquipamentController extends CrudController
 
             //ADMIN OR MANAGER
 
-            if ($request->has('category') && $request->input('category') != '' && $request->has('active') && $request->input('active') == 'true') {
+            if ($request->has('expiration_date') && $request->input('expiration_date') && $request->has('category') && $request->input('category') != '' && $request->has('active') && $request->input('active') == 'true') {
 
                 $productEquipamentAdminSearch = ProductEquipament::with(['category' => function ($query) {
                     $query->whereNull('deleted_at');
@@ -326,7 +326,7 @@ class ProductEquipamentController extends CrudController
                     ->when($request->has('category'), function ($query) use ($request) {
                         $query->where('fk_category_id', $request->input('category'));
                     })
-                    ->where('expiration_date', 0)
+                    ->where('expiration_date',  $request->input('expiration_date'))
                     ->orderBy('fk_category_id', 'asc')
                     ->paginate(10)
                     ->appends(['category' => $request->input('category'), 'active' => $request->input('active')]);
