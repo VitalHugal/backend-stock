aff6ed2dcb5971e6d1b9ba2060e07ba01d80940d
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-01-20 14:45:02 -0300

    Update ProductEquipamentController.php

--- app/Http/Controllers/ProductEquipamentController.php
+++ app/Http/Controllers/ProductEquipamentController.php
@@ -326,7 +326,9 @@ class ProductEquipamentController extends CrudController
                     ->when($request->has('category'), function ($query) use ($request) {
                         $query->where('fk_category_id', $request->input('category'));
                     })
-                    ->where('expiration_date', $request->input('expiration_date'))
+                    ->when($request->has('expiration_date'), function ($query) use ($request) {
+                        $query->where('expiration_date', 'like', '%' . $request->input('expiration_date') . '%');
+                    })
                     ->orderBy('fk_category_id', 'asc')
                     ->paginate(10)
                     ->appends(['category' => $request->input('category'), 'active' => $request->input('active')]);
