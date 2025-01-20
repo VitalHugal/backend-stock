482e4a7f9efa462bb2f49da16495dc909df956a6
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-01-20 14:03:26 -0300

    Update ProductEquipamentController.php

--- app/Http/Controllers/ProductEquipamentController.php
+++ app/Http/Controllers/ProductEquipamentController.php
@@ -119,7 +119,9 @@ class ProductEquipamentController extends CrudController
                         })
 
                         ->whereIn('fk_category_id', $categoryUser)
-                        ->where('fk_category_id', $request->input('category'))
+                        ->when($request->has('category'), function ($query) use ($request) {
+                            $query->where('fk_category_id', 'like', '%' . $request->input('category') . '%');
+                        })
                         ->orderBy('fk_category_id', 'asc')
                         ->paginate(10)
                         ->appends(['category' => $request->input('category'), 'active' => $request->input('active')]);
